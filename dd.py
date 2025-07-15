5d54d725f3ce991d0f0f5373d7f74d72db62a537
Marinakhan <marinakhan384@gmail.com>
2025-07-15 09:16:33 +0500

    Update dd.py

--- dd.py
+++ dd.py
@@ -5,7 +5,6 @@ import bs4
 print('\033[1;94mCHEACKING PKGS')
 os.system('pip install bs4')
 os.system('pkg install speak')
-os.sytem('speak -a 300 WELLCOME TO JAN x JAN COMMAND')
 print('\033[1;94mWELLCOME TO JAN x JAN COMMAND')
 
 

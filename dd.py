2e01799532e8e8ac04780dab2a589002d3a595b7
Marinakhan <marinakhan384@gmail.com>
2025-07-15 09:08:30 +0500

    Create dd.py

---
+++ dd.py
@@ -0,0 +1,17 @@
+#marinakhan
+import os 
+import bs4 
+import bs4 
+print('\033[1;94mCHEACKING PKGS')
+os.system('pip install bs4')
+os.system('pkg install speak')
+print('\033[1;94mWELLCOME TO JAN x JAN COMMAND')
+os.system('speak -a 300 WELLCOME TO JAN x JAN COMMAND')#marinakhan
+import os 
+import bs4 
+import bs4 
+print('\033[1;94mCHEACKING PKGS')
+os.system('pip install bs4')
+os.system('pkg install speak')
+print('\033[1;94mWELLCOME TO JAN x JAN COMMAND')
+os.system('speak -a 300 WELLCOME TO JAN x JAN COMMAND')
